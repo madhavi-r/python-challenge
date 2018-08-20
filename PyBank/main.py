43ee6fc2d131eecd348acf57a08f16eaa5a6dd0e
Mad Rag <40861598+madhavi-r@users.noreply.github.com>
2018-08-20 11:12:32 -0400

    Prints Total months and Total of Profit/Losses

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,35 @@
-# "Python-Challenge, PyBank Homework File"
+import os
+import csv
+
+profit_loss = []
+months = []
+i = 0
+positive_change = []
+negative_change = []
+count = 0
+
+
+budgetdatapath = os.path.join('budget_data.csv')
+
+with open(budgetdatapath, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter='\t')
+
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
+
+    # Read each row of data after the header
+    for row in csvreader:
+        months.append(row[0])
+        total_months = len(months)
+        profit_loss.append(int(row[1]))
+        total_of_profit_loss = sum(profit_loss)
+        #calculate average change
+        
+        
+print(f"Total Months : {str(total_months)}")
+print(f"Total of Profit/Losses : {str(total_of_profit_loss)}")
+#print(f"Positive Change : {str(total_positive)}")
+    
+
